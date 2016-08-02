f8a05186a21de06470033151c2fdae066abbe2d7
steveafrost <ffrostt@gmail.com>
2016-08-02 16:06:20 -0400

    added users controller, signup routes, signup view, login routes, and login view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  get '/signup' do
+    erb :'/users/signup'
+  end
+
+  post '/signup' do
+    @user = User.create(params[:user])
+    session[:user_id] = @user.id
+    redirect to '/'
+  end
+
+  get '/login' do
+    erb :'/users/login'
+  end
+
+  post '/login' do
+    @user = User.find(username: params[:username])
+    session[:user_id] = @user.id
+    redirect to '/'
+  end
+end
