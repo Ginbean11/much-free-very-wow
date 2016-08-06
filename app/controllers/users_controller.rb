ede3ad501ec6808f84a4c857c47dfbdf59e8930c
steveafrost <ffrostt@gmail.com>
2016-08-05 23:03:12 -0400

    added success and error messages for all user routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   get '/signup' do
     if logged_in?
+      flash[:existing_user_error] = "You're already signed up and logged in"
       direct to '/'
     else
       erb :'/users/signup'
@@ -10,8 +11,10 @@ class UsersController < ApplicationController
 
   post '/signup' do
     if blank_params?("user")
+      flash[:signup_error] = "One or more fields were left empty. Please fill in all fields."
       redirect to '/signup'
     else
+      flash[:signup_success] = "Thanks for signing up!"
       @user = User.create(params[:user])
       session[:user_id] = @user.id
       redirect to '/'
@@ -29,14 +32,17 @@ class UsersController < ApplicationController
   post '/login' do
     @user = User.find_by_username(params[:user][:username])
     if @user && @user.authenticate(params[:user][:password])
+      flash[:login_success] = "Welcome, #{@user.username.capitalize}"
       session[:user_id] = @user.id
       redirect to '/'
     else
+      flash[:login_error] = "The login or password entered is invalid"
       redirect to '/login'
     end
   end
 
   get '/logout' do
+    flash[:logout_success] = "Logged out successfully"
     session.clear
     redirect to '/'
   end
